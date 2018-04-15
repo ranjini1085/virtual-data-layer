0526076cafdf7ed157b86210bebd5bc4f30e4e2b
Tony Harper <35902559+tonyharper@users.noreply.github.com>
2018-04-15 18:55:58 -0400

    created function for virtual sql configuration

--- virtual_sql.py
+++ virtual_sql.py
@@ -13,8 +13,47 @@ def virtual_sql(input_sql):
 '''
 
 
-def configure_virtual_sql():
-    None
+def configure_virtual_sql(input_sql_type, target_datastore_type,
+                          target_datastore_url, target_datastore_name):
+    ''' sets the configuration for the virtual sql interface
+
+        keyword_args:
+            input_datastore_type - type of input SQL - Oracle, etc.
+            target_datastore_type - type of target datastore - S3, etc.
+            target_datastore_url - URL of target datastore, if applicable
+                for a database, this should be the database URL
+                for S3, this should be 'None'
+            target_datastore_name - name of target datastore
+                for a database, this should be the database name
+                for S3, this should be the bucket name
+
+        returns:
+            True if successful
+            False if not
+
+'''
+
+    # write configuration to virtuql_sql_configuration.txt
+    try:
+        with open('virtual_sql_configuration.txt', mode='w') as config_file:
+            config_file.write('input_sql_type=' +
+                              input_sql_type + '\n')
+            config_file.write('target_datastore_type=' +
+                              target_datastore_type + '\n')
+            config_file.write('target_datastore_url=' +
+                              target_datastore_url + '\n')
+            config_file.write('target_datastore_name=' +
+                              target_datastore_name + '\n')
+
+    except TypeError as te:
+        print(te)
+        return False
+
+    except PermissionError as pe:
+        print(pe)
+        return False
+
+    return True
 
 
 def connect_to_database(target_datastore_type,
@@ -39,11 +78,18 @@ def connect_to_database(target_datastore_type,
         return cursor
 
     elif target_datastore_type == 'oracle':
-        conn_string = target_datastore_username + '/' + \
-            target_datastore_password + "@" + target_datastore_url + "/" + \
-            target_datastore_dbname
+        # conn_string = target_datastore_username + '/' + \
+        #    target_datastore_password + "@" + target_datastore_url + "/" + \
+        #    target_datastore_dbname
+
+        print('Oracle not yet supported')
 #        conn = cx_Oracle.connect(conn_string)
 #        print(conn.version)
 #        conn.close
 
         return None
+
+
+if __name__ == '__main__':
+
+    print(configure_virtual_sql('Oracle', 'S3', 'None', 'virtual-data-layer'))
